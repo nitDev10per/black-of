11e0c1f583f6cb3874719a37501fc5418847feeb
harshita-bhoomicam <harshita.srivastava@bhoomicam.com>
2025-05-26 09:43:54 +0530

    make responive

--- src/screens/AutomotiveUpdate/sections/ServicesSection.tsx
+++ src/screens/AutomotiveUpdate/sections/ServicesSection.tsx
@@ -24,7 +24,7 @@ export const ServicesSection = (): JSX.Element => {
 
   useEffect(() => {
     if (hovered) {
-      setDaynamicLoad(true)
+      setDaynamicLoad(true);
     }
   }, [hovered]);
 
@@ -171,149 +171,173 @@ export const ServicesSection = (): JSX.Element => {
           </Box>
         </Typography>
 
-        {daynamicLoad? <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "90vw",
-            position: "relative",
-            transform: !hovered ? "translateY(300px)" : "translateY(0)",
-            opacity: !hovered ? 0 : 1,
-            transition: "transform 0.7s ease-out, opacity 0.5s ease-out",
-          }}
-        >
-          {/* scroll bar */}
-          <Box
-            sx={{
-              width: "2px",
-              margin: "70px 0",
-              height: "30vh",
-              backgroundColor: "gray",
-              position: "relative",
-            }}
-          >
-            <Box
-              sx={{
-                width: "2px",
-                height: currentIndex === 0 ? "100%" : "50%",
-                backgroundColor: "white",
-                position: "absolute",
-                top: 0,
-                left: "0%",
-                transition: "height 0.5s ease-out",
-                // overflow: 'visible',
-              }}
-            ></Box>
-          </Box>
-
-          {/* video section */}
+        {daynamicLoad ? (
           <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
-              alignItems: "start",
-              justifyContent: "center",
-              width: "100%",
-              padding: 2,
-              position: "absolute",
-              zIndex: 0,
-              transform:
-              !hovered? '"translateY(200%)"' : currentIndex === 0 ? "translateY(-50%)" : "translateY(0)",
-              transition: "transform 0.5s ease-out",
-              left: "10%",
+              justifyContent: "space-between",
+              width: "90vw",
+              position: "relative",
+              transform: !hovered ? "translateY(300px)" : "translateY(0)",
+              opacity: !hovered ? 0 : 1,
+              transition: "transform 0.7s ease-out, opacity 0.5s ease-out",
             }}
           >
-            {/* //video section */}
-            {currentVideo.map((video, index) => (
+            {/* scroll bar */}
+            <Box
+              sx={{
+                width: "2px",
+                margin: "70px 0",
+                height: "30vh",
+                backgroundColor: "gray",
+                position: "relative",
+              }}
+            >
               <Box
-              key={index}
                 sx={{
-                  display: "flex",
-                  height: "auto",
-                  overflow: "hidden",
-                  opacity: currentIndex === index ? 0 : 1,
-                  transition: "opacity 0.5s ease-out",
-                  flexWrap: "wrap",
+                  width: "2px",
+                  height: currentIndex === 0 ? "100%" : "50%",
+                  backgroundColor: "white",
+                  position: "absolute",
+                  top: 0,
+                  left: "0%",
+                  transition: "height 0.5s ease-out",
+                  // overflow: 'visible',
                 }}
-              >
+              ></Box>
+            </Box>
+
+            {/* video section */}
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+                justifyContent: "center",
+                width: "100%",
+                padding: 2,
+                position: "absolute",
+                zIndex: 0,
+                transform: !hovered
+                  ? '"translateY(200%)"'
+                  : currentIndex === 0
+                  ? "translateY(-50%)"
+                  : "translateY(0)",
+                transition: "transform 0.5s ease-out",
+                left: "10%",
+              }}
+            >
+              {/* //video section */}
+              {currentVideo.map((video, index) => (
                 <Box
+                  key={index}
                   sx={{
                     display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "max-content",
-                    gap: 10,
+                    height: "auto",
+                    overflow: "hidden",
+                    opacity: currentIndex === index ? 0 : 1,
+                    transition: "opacity 0.5s ease-out",
+                    flexWrap: "wrap",
                   }}
                 >
                   <Box
                     sx={{
                       display: "flex",
-                      flexDirection: "column",
-                      alignItems: "start",
-                      gap: 50,
-                      maxWidth: 500,
-                      paddingRight: 2,
+                      alignItems: "center",
+                      justifyContent: "center",
+                      width: "max-content",
+                      gap: 10,
                     }}
                   >
-                    <Typography
-                      variant="h6"
-                      color="white"
+                    <Box
                       sx={{
-                        maxWidth: 500,
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "start",
-                        gap: 1,
-                        fontSize: { xs: "0.6rem", sm: "0.7rem", md: "1rem", lg: "1.3rem" },
+                        gap: 50,
+                        maxWidth: 500,
+                        paddingRight: 2,
                       }}
                     >
-                      <Box component="span" fontWeight="bold">
-                        {video.title}
-                        <br />
-                      </Box>
-                      <Box
-                        component="span"
-                        fontWeight="normal"
-                        sx={{ opacity: 0.8 }}
+                      <Typography
+                        variant="h6"
+                        color="white"
+                        sx={{
+                          maxWidth: 500,
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "start",
+                          gap: 1,
+                          fontSize: {
+                            xs: "0.6rem",
+                            sm: "0.7rem",
+                            md: "1rem",
+                            lg: "1.3rem",
+                          },
+                        }}
                       >
-                        {video.description}
-                      </Box>
-                    </Typography>
+                        <Box component="span" fontWeight="bold">
+                          {video.title}
+                          <br />
+                        </Box>
+                        <Box
+                          component="span"
+                          fontWeight="normal"
+                          sx={{ opacity: 0.8 }}
+                        >
+                          {video.description}
+                        </Box>
+                      </Typography>
+                    </Box>
+                  </Box>
+                  {/* Video Box */}
+                  <Box
+                    sx={{
+                      minHeight: {
+                        xs: "50vw",
+                        sm: "40vw",
+                        md: "26vw",
+                        lg: "20vw",
+                      },
+                    }}
+                  >
+                    <video
+                      ref={index === 0 ? videoRef : null}
+                      autoPlay
+                      loop
+                      muted
+                      playsInline
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        maxHeight: !hovered
+                          ? "5vw"
+                          : index === 1 && currentIndex === 0
+                          ? getWidth()
+                          : index === 0 && currentIndex === 1
+                          ? getWidth()
+                          : "5vw",
+                        maxWidth: "100%",
+                        transition:
+                          "max-height 0.5s ease-out, max-width 0.5s ease-out",
+                      }}
+                    >
+                      <source src={video.src} type="video/mp4" />
+                      Your browser does not support the video tag.
+                    </video>
                   </Box>
                 </Box>
-                 {/* Video Box */}
-          <Box sx={{ minHeight: { xs: "50vw", sm: "40vw", md: "26vw", lg: "20vw" }, }}>
-            <video
-              ref={index === 0 ? videoRef : null}
-              autoPlay
-              loop
-              muted
-              playsInline
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                maxHeight: !hovered
-                  ? "5vw"
-                  : index === 1 && currentIndex === 0
-                  ? getWidth()
-                  : index === 0 && currentIndex === 1
-                  ? getWidth()
-                  : "5vw",
-                maxWidth: "100%",
-                transition: "max-height 0.5s ease-out, max-width 0.5s ease-out",
-              }}
-            >
-              <source src={video.src} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+              ))}
+            </Box>
           </Box>
-              </Box>
-            ))}
-          </Box>
-        </Box> : <Box sx={{
-          height: "30vh",
-        }}></Box>}
+        ) : (
+          <Box
+            sx={{
+              height: "30vh",
+            }}
+          ></Box>
+        )}
 
         <Typography
           sx={{
@@ -336,35 +360,48 @@ export const ServicesSection = (): JSX.Element => {
             alignItems: "center",
             gap: 4,
             mt: 1,
-            width: "90vw",
+            width: isXs? '100vw' : "90vw",
             zIndex: 12,
           }}
         >
-          {cabinData?.map((item, index) => (
-            <ClickableCard
-              key={index}
-              imageSrc={item.imageSrc}
-              alt={item.alt}
-              text={item.text}
-              onClick={() => handleClickParts(item.video)}
-              isActive={currentVideo[0].src === item.video}
-              style={{
-                opacity: !hovered ? 0 : currentIndex === 0 ? 0 : 1,
-                transform:
-                !hovered ? "translateY(50px)" : currentIndex === 0 ? "translateY(50px)" : "translateY(0)",
-                transition:
-                  "opacity 0.5s ease-in-out, transform 0.5s ease-in-out",
-              }}
-            />
-          ))}
+          <Box sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "end",
+            alignItems: "center",
+            gap: 4,
+            zIndex: 12,
+          }}>
+            {cabinData?.map((item, index) => (
+              <ClickableCard
+                key={index}
+                imageSrc={item.imageSrc}
+                alt={item.alt}
+                text={item.text}
+                onClick={() => handleClickParts(item.video)}
+                isActive={currentVideo[0].src === item.video}
+                style={{
+                  opacity: !hovered ? 0 : currentIndex === 0 ? 0 : 1,
+                  transform: !hovered
+                    ? "translateY(50px)"
+                    : currentIndex === 0
+                    ? "translateY(50px)"
+                    : "translateY(0)",
+                  transition:
+                    "opacity 0.5s ease-in-out, transform 0.5s ease-in-out",
+                }}
+              />
+            ))}
+            
+          </Box>
           <Image
-            src={"/img/playIcon.png"}
-            alt={"Play Icon"}
-            width={100}
-            height={100}
-            style={{ borderRadius: 8, width: "10vh", height: "10vh" }}
-            loading="lazy"
-          />
+              src={"/img/playIcon.png"}
+              alt={"Play Icon"}
+              width={100}
+              height={100}
+              style={{ borderRadius: 8, width: "10vh", height: "10vh" }}
+              loading="lazy"
+            />
         </Box>
       </Container>
     </Box>
